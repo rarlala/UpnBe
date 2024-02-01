f7a0366e3198f4811db17fb3faca29b91288df1f
rarlala <wtw9611@gmail.com>
2024-02-01 16:04:10 +0900

    Initial Commit

---
+++ UpnBe/UpnBeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpnBeApp.swift
+//  UpnBe
+//
+//  Created by Rarla on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct UpnBeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
